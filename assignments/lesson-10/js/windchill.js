5f61a1c2bfe1fe4d08bf898b24026b7ae8559942
CJT10 <cassidyc@byui.edu>
2019-06-25 08:12:22 -0500

    update

--- assignments/lesson-10/js/windchill.js
+++ assignments/lesson-10/js/windchill.js
@@ -1,7 +1,7 @@
 
 
 
-var t = parseInt(document.getElementById("temp").innerHTML);
+var t = parseInt(document.getElementById('current-temp').innerHTML);
 var s = parseInt(document.getElementById("windspeed").innerHTML);
 var chill = (35.74 + (0.6215*t) - 35.75* Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
 var c = chill.toFixed(1);
