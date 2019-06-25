e2999cbd48af9535fb70b14ba77ba54f84872d45
CJT10 <cassidyc@byui.edu>
2019-06-24 20:11:04 -0500

    lesson 10 update

---
+++ assignments/lesson-10/js/weatherapi.js
@@ -0,0 +1,11 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = "//api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c56d49a01e77b339a90a3bf2c7dcf667";
+
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText)
+    console.log(weatherData);
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+}
