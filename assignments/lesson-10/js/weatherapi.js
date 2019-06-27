2c8de65e5706a84678a4de5d5b990f8cf9b4ab48
CJT10 <cassidyc@byui.edu>
2019-06-26 19:58:17 -0500

    update

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -1,14 +1,14 @@
 let weatherRequest = new XMLHttpRequest();
-let apiURLstring = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c56d49a01e77b339a90a3bf2c7dcf667";
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c56d49a01e77b339a90a3bf2c7dcf667';
 
 weatherRequest.open('Get', apiURLstring, true);
 weatherRequest.send();
 
 weatherRequest.onload =  function () {
-    let weatherData = JSON.parse(weatherRequest.responseText)
+    let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
     document.getElementById('description').innerHTML = weatherData.weather[0].description;
-    document.getElementById('high').innerHTML = weatherData.main.temp;
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
     document.getElementById('humidity').innerHTML = weatherData.main.temp_max;
     document.getElementById('windspeed').innerHTML = weatherData.wind.speed;
 
