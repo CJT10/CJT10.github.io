db94e149c1923b2e55dacea11daf8a489187f073
CJT10 <cassidyc@byui.edu>
2019-06-28 10:47:29 -0500

    UPDATE

--- assignments/lesson-10/js/weatherapi.js
+++ assignments/lesson-10/js/weatherapi.js
@@ -1,5 +1,6 @@
 let weatherRequest = new XMLHttpRequest();
-let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c56d49a01e77b339a90a3bf2c7dcf667';
+let apiURLstring =
+  "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c56d49a01e77b339a90a3bf2c7dcf667";
 
 weatherRequest.open('Get', apiURLstring, true);
 weatherRequest.send();
